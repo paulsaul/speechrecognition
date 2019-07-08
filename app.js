6d361270961c19557c4bc216be6e7d9a51b4582c
paulwababu <44939805+paulwababu@users.noreply.github.com>
2019-07-08 06:35:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -85,6 +85,41 @@ const doi = [
     'Making time to go outside on a nice day also delivers a huge advantage; one study found that spending 20 minutes outside in good weather not only boosted positive mood, but broadened thinking and improved working memory'
 
 ];
+const hope = [
+      'cheer up my child, as your father once said, heaven has got a plan for you'
+      ,'when all hope is gone, is left no fear. Do not despair for even in the darkest moments there is always something positive out of it.'
+];
+const predict = [
+     'no, but from the questions you ask me, i can tell that you '
+];
+const quote = [
+   'Repeat after me. I am the master of my fate. I am the captain of my soul'
+    ,'We must have perseverance and, above all, confidence in ourselves. We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained'
+    ,'You gain strength, courage and confidence by every experience in which you really stop to look fear in the face . . . You must do the thing you think you cannot do'
+];
+const sorry = [
+    'With everything that has happened to you, you can either feel sorry for yourself or treat what has happened as a gift. Everything is either an opportunity to grow or an obstacle to keep you from growing. You get to choose'
+
+];
+const jealous = [
+     'Jealousy is a disease, love is a healthy condition. The immature mind often mistakes one for the other, or assumes that the greater the love, the greater the jealousy - in fact, they are almost incompatible; one emotion hardly leaves room for the other.'
+     ,'What sort of love is permeated by jealousy? You are jealous because you are unaware that everything you need is inside you.'
+    ,'A competent and self-confident person is incapable of jealousy in anything. Jealousy is invariably a symptom of neurotic insecurity.'
+];
+const hate = [
+    'Jealousy is a disease, love is a healthy condition. The immature mind often mistakes one for the other, or assumes that the greater the love, the greater the jealousy - in fact, they are almost incompatible; one emotion hardly leaves room for the other.'
+    ,'It is easy to hate and it is difficult to love. This is how the whole scheme of things works. All good things are difficult to achieve; and bad things are very easy to get.'
+  
+];
+const real = [
+       'i think so. what about you?'
+];
+const betray = [
+    'I know things are really rough for you right now, but that will not cause me to think of leaving you. We are in this together love'
+     ,'I wish there is a way I could reassure you of your doubts and fears, so you will know that I am still here. I am going nowhere'
+];
+
+
 
 
 //add speech recognition
@@ -245,14 +280,50 @@ function readOutLoud(message){
                      speech.text = finalText20;
     }
     if(message.includes('what can i do to be happy')) {
-        const finalText20 =
+        const finalText21 =
                      doi[Math.floor(Math.random() * doi.length)];
-                     speech.text = finalText20;
+                     speech.text = finalText21;
     }
     if(message.includes('are you loyal')) {
-        const finalText20 =
-                     name[Math.floor(Math.random() * name.length)];
-                     speech.text = finalText20;
+        const finalText22 =
+                     loyal[Math.floor(Math.random() * loyal.length)];
+                     speech.text = finalText22;
+    }
+    if(message.includes('encourage me all hope is gone')) {
+        const finalText23 = 
+              hope[Math.floor(Math.random() * hope.length)];
+              speech.text = finalText23;
+    
+    }
+    if(message.includes('can you read horoscopes')) {
+        const finalText24 = 
+               predict[Math.floor(Math.random() * predict.length)];
+               speech.text = finalText24;
+    }
+    if(message.includes('tell me something')) {
+       const finalText25 = 
+              quote[Math.floor(Math.random() * quote.length)];
+               speech.text = finalText25;
+    }
+    if(message.includes('jealousy has clouded my judgement')) {
+         const finalText26 = 
+               jealous[Math.floor(Math.random() * jealous.length)];
+               speech.text = finalText26; 
+    }
+    if(message.includes('hate has clouded my judgement')) {
+        const finalText27 = 
+              hate[Math.floor(Math.random() * hate.length)];
+              speech.text = finalText27;
+    }
+    if(message.includes('are you real')) {
+        const finalText28 = 
+              real[Math.floor(Math.random() * real.length)];
+               speech.text = finalText28;
+    }
+    if(message.includes('never betray me')) {
+       const finalText29 = 
+             betray[Math.floor(Math.random() * betray.length)];
+             speech.text = finalText29;
     }
    
 
